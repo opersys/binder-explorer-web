73955cc886bfd1d2eb7daa4736e6b823f0fb1e97
François-Denis Gonthier <francois-denis.gonthier@opersys.com>
2015-04-17 09:19:39 -0400

    Remove sigma.js code.

--- src/js/config.js
+++ src/js/config.js
@@ -20,18 +20,6 @@ requirejs.config({
             deps: ["jquery"],
             exports: "w2ui"
         },
-        "sigma": {
-            exports: "sigma"
-        },
-        "sigma.layout.forceAtlast2": {
-            deps: ["sigma"]
-        },
-        "sigma.plugins.dragNodes": {
-            deps: ["sigma"]
-        },
-        "sigma.plugins.customEdgeShapes": {
-            deps: ["sigma"]
-        },
         "jquery-timer/jquery.timer": {
             deps: ["jquery"]
         },
@@ -50,10 +38,6 @@ requirejs.config({
     },
     paths: {
         w2ui: "w2ui/w2ui-1.4.2.min",
-/*        sigma: "sigma.js/sigma.min",
-        "sigma.layout.forceAtlast2": "sigma.js/plugins/sigma.layout.forceAtlas2.min",
-        "sigma.plugins.dragNodes": "sigma.js/plugins/sigma.plugins.dragNodes.min",
-        "sigma.plugins.customEdgeShapes": "sigma.js/plugins/sigma.renderers.customEdgeShapes.min",*/
         underscore: "underscore/underscore",
         backbone: "backbone/backbone",
         jquery: "jquery/jquery",
