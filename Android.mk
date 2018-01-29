6c141095b12adff6c737b236ba550ab8166af036
François-Denis Gonthier <fdgonthier@lostwebsite.net>
2018-01-29 14:52:16 -0500

    Added Android.mk and launch script OsysBE for integration into AOSP build.

---
+++ Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := OsysBE
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := OsysBE
+LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
+
+LOCAL_POST_INSTALL_CMD := \
+	mkdir -p $(TARGET_OUT)/Osys/BE; \
+	cp -af $(LOCAL_PATH)/dist_arm64/* $(TARGET_OUT)/Osys/BE
+include $(BUILD_PREBUILT)
+
