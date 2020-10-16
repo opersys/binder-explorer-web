39dd3bbacaa354e4b3cc267a51606c6991bc8ed9
François-Denis Gonthier <francois-denis.gonthier@opersys.com>
2020-10-16 17:21:27 -0400

    Convert WSHandler to ES6 class

--- src/js/wshandler.js
+++ src/js/wshandler.js
@@ -17,65 +17,57 @@
 "use strict";
 
 define(function (require) {
-    var Process = require("models/Process");
+    const Process = require("models/Process");
 
-    var WSHandler = function (sock, binderServices, binderProcesses) {
-        var self = this;
+    return class WSHandler {
+        constructor(sock, binderServices, binderProcesses) {
+            this._sock = sock;
+            this._binderServices = binderServices;
+            this._binderProcesses = binderProcesses;
 
-        self._sock = sock;
-        self._binderServices = binderServices;
-        self._binderProcesses = binderProcesses;
+            this._sock.on("processadded", (data) => {
+                this._onProcessAdded(data);
+            });
 
-        self._sock.on("processadded", function (data) {
-            self._onProcessAdded.apply(self, [data]);
-        });
+            this._sock.on("processremoved", (data) => {
+                this._onProcessRemoved(data);
+            });
 
-        self._sock.on("processremoved", function (data) {
-            self._onProcessRemoved.apply(self, [data]);
-        });
+            this._sock.on("processserviceadded", (data) => {
+                this._onProcessServiceAdded(data);
+            });
 
-        self._sock.on("processserviceadded", function (data) {
-            self._onProcessServiceAdded.apply(self, [data]);
-        });
+            this._sock.on("processserviceremoved", (data) => {
+                this._onProcessServiceRemoved(data);
+            });
 
-        self._sock.on("processserviceremoved", function (data) {
-            self._onProcessServiceRemoved.apply(self, [data]);
-        });
+            this._sock.on("service", (data) => {
+                this._onService(data);
+            });
+        }
 
-        self._sock.on("service", function (data) {
-            self._onService.apply(self, [data]);
-        });
-    };
+        _onProcessServiceAdded(userService) {
+            if (this._binderProcesses.get(userService.pid) != null)
+                this._binderProcesses.get(userService.pid).addUserService(userService);
+        }
 
-    WSHandler.prototype._onProcessServiceAdded = function (userService) {
-        var self = this;
+        _onProcessServiceRemoved(userService) {
+            if (this._binderProcesses.get(userService.pid) != null)
+                this._binderProcesses.get(userService.pid).removeUserService(userService);
+        }
 
-        if (self._binderProcesses.get(userService.pid) != null) {
-            self._binderProcesses.get(userService.pid).addUserService(userService);
+        _onProcessAdded(binderProcess) {
+            binderProcess.process = new Process(binderProcess.process);
+            this._binderProcesses.add(binderProcess);
+        }
+
+        _onProcessRemoved(binderProcessPid) {
+            this._binderProcesses.remove(binderProcessPid);
+        }
+
+        _onService(binderService) {
+            binderService.process = new Process(binderService.process);
+            this._binderServices.add(binderService);
         }
     };
-
-    WSHandler.prototype._onProcessServiceRemoved = function (userService) {
-        var self = this;
-
-        if (self._binderProcesses.get(userService.pid) != null) {
-            self._binderProcesses.get(userService.pid).removeUserService(userService);
-        }
-    };
-
-    WSHandler.prototype._onProcessAdded = function (binderProcess) {
-        binderProcess.process = new Process(binderProcess.process);
-        this._binderProcesses.add(binderProcess);
-    };
-
-    WSHandler.prototype._onProcessRemoved = function (binderProcessPid) {
-        this._binderProcesses.remove(binderProcessPid);
-    };
-
-    WSHandler.prototype._onService = function (binderService) {
-        binderService.process = new Process(binderService.process)
-        this._binderServices.add(binderService);
-    };
-
-    return WSHandler;
 });
